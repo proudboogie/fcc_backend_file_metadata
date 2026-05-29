23f36c2bdf75f7cb2e5bba2e15ac4175216f5236
Israel Perez <isra.perez.2000@gmail.com>
2026-05-29 12:37:40 +0200

    test: request and response log
    fix: rebase main

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// view requests
+app.use(function(req, res, next){
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +24,9 @@ app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
   const name = req.file.originalname;
   const type = req.file.mimetype;
   const size = req.file.size;
+
+  // view responses
+  console.log("name", name,"\n", "type",type, "\n", "size",size)
 
   res.json({name:name, type:type, size:size});
 });
